96d800fcd1ccf3a1e141ec4d03507cf382a34eb5
Mrzhailiming <401875751@qq.com>
2021-09-03 18:42:00 +0800

    base

---
+++ SocketServer/SocketServerHelper_Interface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketServer
+{
+    public interface SocketServerHelper_Interface
+    {
+        TcpListener tcpListener = new TcpListener(new IPEndPoint(IPAddress.Any, 13000));
+    }
+}
---
+++ SocketServer/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imgPath = "";
+            byte[] bArr;
+            //读源jpg
+            using (FileStream oldFileStream = new FileStream(imgPath, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+            {
+                bArr = new byte[oldFileStream.Length];
+                oldFileStream.Read(bArr, 0, bArr.Length);
+                oldFileStream.Close();
+            }
+                
+            //写入新的文件
+            using (FileStream newFileStre = new FileStream($"{imgPath}_new.jpg", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
+            {
+                newFileStre.Write(bArr, 0, bArr.Length);
+            }
+        }
+    }
+}
