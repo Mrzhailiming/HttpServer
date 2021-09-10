f1955fec70aa66df272c92534133171dc3423d43
Mrzhailiming <401875751@qq.com>
2021-09-10 10:56:53 +0800

    信息： 1.异步处理task 问题： 1.出现两个线程使用一个socket问题

--- common/FileHelper/socket/CMDDispatcher.cs
+++ common/FileHelper/socket/CMDDispatcher.cs
@@ -2,8 +2,11 @@
 using Helper;
 using SocketServer.tool;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace Helper
 {
@@ -14,7 +17,72 @@ namespace Helper
     }
     public class CMDDispatcher : Singletion<CMDDispatcher>
     {
+        static int threadNum = 4;
+        static int semaphoreMaximumCount = 100;//100个不够吧
+        Semaphore semaphore = new Semaphore(0, semaphoreMaximumCount);
+        static int count = 0;
+
         Dictionary<TCPCMDS, CMDHandler> _CMD2Action = new Dictionary<TCPCMDS, CMDHandler>();
+        ConcurrentQueue<TCPTask> _taskQueue = new ConcurrentQueue<TCPTask>();
+        CMDDispatcher()
+        {
+            for(int i = 0; i < threadNum; ++i)
+            {
+                Thread thread = new Thread(Execute);
+                thread.Name = i.ToString();
+                thread.Start();
+            }
+        }
+
+        public void Test()
+        {
+            //for(int i = 0; i < 101; ++i)
+            //{
+            //    TCPTask task = new TCPTask();
+            //    Dispatcher(task);
+            //}
+        }
+        private void Execute()
+        {
+            TCPTask task = null;
+            CMDHandler outHandler;
+            TCPCMDS cmdID;
+            //使用生产者消费者模型?
+            while (true)
+            {
+                try
+                {
+                    semaphore.WaitOne();
+                    //
+                    if (_taskQueue.TryDequeue(out task))
+                    {
+                        cmdID = (TCPCMDS)Byte4Int(task.buffer);
+
+                        if (!_CMD2Action.TryGetValue(cmdID, out outHandler))
+                        {
+                            LogHelper.Log(LogType.Error_CMDIsNull, cmdID.ToString());
+                        }
+                        else
+                        {
+                            LogHelper.Log(LogType.Msg_ProcessCmd, cmdID.ToString());
+
+                            outHandler._action(task);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogHelper.Log(LogType.Exception, ex.ToString());
+                }
+                finally
+                {
+                    task = null;
+                    outHandler = null;
+                    Interlocked.Increment(ref count);
+                    Console.WriteLine("threadName:{0}__taskID:{1}", Thread.CurrentThread.Name, count);
+                }
+            }
+        }
         public void RegisterCMD(TCPCMDS cmdID, Action<TCPTask> action)
         {
             CMDHandler newHandler = new CMDHandler()
@@ -33,20 +101,15 @@ namespace Helper
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="task"></param>
         public void Dispatcher(TCPTask task)
         {
-            TCPCMDS cmdID = (TCPCMDS)Byte4Int(task.buffer);
-            CMDHandler outHandler;
-            if (!_CMD2Action.TryGetValue(cmdID, out outHandler))
-            {
-                LogHelper.Log(LogType.Error_CMDIsNull, cmdID.ToString());
-            }
-            else
-            {
-                LogHelper.Log(LogType.Msg_ProcessCmd, cmdID.ToString());
-                
-                outHandler._action(task);
-            }
+            _taskQueue.Enqueue(task);
+
+            semaphore.Release();
         }
 
 
---
+++ common/Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using Helper;
+using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CMDDispatcher dispatcher = new CMDDispatcher();
+            dispatcher.Test();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
